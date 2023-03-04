9ea9fe82fb122e525f0475266827f82f355595d3
wwwbijay <link2bj@gmail.com>
2023-03-04 16:49:14 +0545

    server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve("frontend", "index.html"));
+});
+
+app.listen(process.env.PORT || 5080, () => {
+  console.log("server running...");
+});
